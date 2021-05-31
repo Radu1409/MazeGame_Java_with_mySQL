9c3d67ef72dbca00c23c104bc44071ab22937fde
Lenovo <radu.purecel20@gmail.com>
2021-05-31 22:21:13 +0300

    Initial Commit

--- src/mazeproject/IDandPass.java
+++ src/mazeproject/IDandPass.java
@@ -1,5 +1,6 @@
 package mazeproject;
 
+import java.sql.*;
 import java.util.HashMap;
 
 public class IDandPass {
@@ -8,9 +9,22 @@ public class IDandPass {
 
     IDandPass() {
 
+        String url = "jdbc:mysql://localhost:3306/utilizatori";
+        String user = "root";
+        String password = "123456";
 
-        logininfo.put("Radu", "boss");
-        logininfo.put("george", "enescu");
+        try {
+            Connection myCon = DriverManager.getConnection(url, user, password);
+            Statement statement = myCon.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from tabel1");
+
+            while (resultSet.next()) {
+                logininfo.put(resultSet.getString("utilizator"), resultSet.getString("parola"));
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     protected HashMap getlogininfo() {
--- src/mazeproject/Main.java
+++ src/mazeproject/Main.java
@@ -1,5 +1,6 @@
 package mazeproject;
 
+
 public class Main {
     public static void main(String[] args) {
         new Start();
